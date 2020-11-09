088b52b699ab3dc025f4e0f2d9e126cd09afcc21
Coder_WangChneyang <1351093262@qq.com>
2020-11-09 15:55:42 +0800

    第八天微服务网关和JWT令牌

--- changgou_parent/changgou_gateway/changgou_gateway_web/src/main/java/com/changgou/GatewayWebApplication.java
+++ changgou_parent/changgou_gateway/changgou_gateway_web/src/main/java/com/changgou/GatewayWebApplication.java
@@ -2,12 +2,32 @@ package com.changgou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
 
 @SpringBootApplication
 @EnableEurekaClient
 public class GatewayWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(GatewayWebApplication.class,args);
+    }
+
+    /***
+     * IP限流
+     * @return
+     */
+    @Bean(name = "ipKeyResolver")
+    public KeyResolver userKeyResolver(){
+        return new KeyResolver() {
+            @Override
+            public Mono<String> resolve(ServerWebExchange exchange) {
+                //获取远程客户端IP
+                String hostName = exchange.getRequest().getRemoteAddress().getAddress().getHostAddress();
+                return Mono.just(hostName);
+            }
+        };
     }
 }
