2cf3dc374034f806b874ed209e575b3519c9bd7f
Coder_WangChneyang <1351093262@qq.com>
2020-11-09 15:26:29 +0800

    第七天Thymeleaf、Rabbitmq实现静态页

---
+++ changgou_parent/changgou_gateway/changgou_gateway_web/src/main/java/com/changgou/GatewayWebApplication.java
@@ -0,0 +1,13 @@
+package com.changgou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class GatewayWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayWebApplication.class,args);
+    }
+}
